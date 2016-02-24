99d7943682c151d9305f5487410375638f1090e8
Matt Miller <thisismattmiller@gmail.com>
2016-02-24 12:02:00 -0500

    fixed agentstest name

---
+++ jobs/google_vision.js
@@ -0,0 +1,237 @@
+var request = require('request')
+var fs      = require('fs')
+
+
+
+var url = "http://images.nypl.org/index.php?id=815625&t=w"
+var api = "https://vision.googleapis.com/v1/images:annotate?key={KEYKEYKEYKEYKEY}"
+
+request(url, {encoding: 'binary'}, function(error, response, body) {
+  	
+	var image = new Buffer(body.toString(), "binary").toString("base64")
+
+	request.post(api,
+		{
+			requests:[
+				{
+					"image":{
+						"content":image
+					},
+					"features":[
+						{
+						"type":"LABEL_DETECTION",
+						"maxResults":5
+						}
+					]
+				}
+			]
+		},
+		function (e, r, body) {
+
+			console.log(e)
+			console.log(body)
+
+
+
+		})
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cluster = require('cluster');
+
+// if (cluster.isMaster) {
+
+// 	var db = require("../lib/db.js")
+// 	var utils = require("../lib/utils.js")
+// 	var config = require("config")
+// 	var blessed = require('blessed')
+// 	var contrib = require('blessed-contrib')
+// 	//var screen = blessed.screen()
+	
+// 	var botCount = 15, activeBot = 0
+// 	var total = 0
+
+// 	db.prepareAgentsIndex(function(){
+
+// 		//find out how many agents there are total
+// 		db.returnCollectionTripleStore("agents",function(err,agentsCollection){
+// 			agentsCollection.count(function(err,count){
+
+// 				//break up how much to work for each bot
+// 				var perBot = Math.floor(count / botCount)
+
+// 				//index 0
+// 				var start = perBot*-1
+
+// 				console.log("perBot",perBot)
+
+// 				var buildWorker = function(){
+// 					activeBot++
+			
+// 					start = start + perBot
+
+// 					var worker = cluster.fork();
+
+// 					worker.on('message', function(msg) {
+// 						if (msg.start) {
+// 							worker.send({ start: start , total : perBot  })
+// 						}
+
+// 						if (msg.totalUpdate){
+// 							total = total + parseInt(msg.totalUpdate)
+// 							console.log(total)
+// 						}
+
+
+// 					})
+// 				}
+
+// 				var interval = setInterval(function(){
+// 					if (activeBot==botCount){
+// 						clearInterval(interval)
+// 						return false
+// 					}
+// 					buildWorker()
+
+// 				},30000)
+
+
+// 			})
+// 		})
+// 	})
+
+// 	cluster.on('disconnect', function(worker, code, signal) {		
+// 		console.log("There are",Object.keys(cluster.workers).length,"Bots")
+// 	})
+
+// } else {
+
+// 	var db = require("../lib/db.js")
+// 	var _ = require('highland')
+// 	var utils = require("../lib/utils.js")
+// 	var total = 0
+
+// 	var skipRebuildResources = true
+
+// 	var addResourceStats = _.wrapCallback(function addTopFiveSubject(agent,cb){
+
+
+// 		if (skipRebuildResources && agent.topFiveTerms && agent.topFiveRoles && agent.useCount){
+// 			//if enabled we will just use the existing data
+// 			cb(null,agent)
+
+// 		}else{
+
+// 			//rebuild
+
+// 			db.returnCollectionTripleStore("resources",function(err,resourcesCollection){
+
+// 				var allTerms = []
+// 				var allRoles = []
+// 				var totalResources = 0
+
+// 				_(resourcesCollection.find({allAgents: agent.uri}).stream())
+// 					.map(r =>{
+// 						allTerms.push(utils.extractTerms(r,agent.uri))
+// 						allRoles.push(utils.extractRoles(r,agent.uri))
+// 						totalResources++
+// 					}).done(function(){
+
+// 						allTerms = utils.topFiveTerms(allTerms)
+// 						allRoles = utils.topFiveTerms(allRoles)
+// 						agent.topFiveTerms = allTerms
+// 						agent.topFiveRoles = allRoles
+// 						agent.useCount = totalResources
+
+// 						cb(err,agent)
+// 					})
+// 			})
+// 		}
+// 	})
+
+// 	var updateAgentsCollection = _.wrapCallback(function updateAgentsCollection(agent,cb){
+// 		db.returnCollectionTripleStore("agents",function(err,agentsCollection){
+// 			agentsCollection.update({ uri:agent.uri }, {$set: { topFiveTerms: agent.topFiveTerms, topFiveRoles: agent.topFiveRoles, useCount: agent.useCount } },function(err,res){
+// 				if (err) console.log(err)
+// 				cb(err,agent)
+// 			})
+// 		})
+// 	})
+
+// 	db.returnCollectionTripleStore("agents",function(err,agentsCollection){
+// 		//ask for where to start
+// 		process.send({ start: true })
+
+// 		process.on('message', function(msg){
+// 			if (typeof msg.start === 'number'){
+// 				console.log(cluster.worker.id, " Starting at",msg.start, "Limit ",msg.total )
+// 				_(agentsCollection.find({}).skip(parseInt(msg.start)).limit(msg.total).batchSize(100).stream())
+// 					.map(utils.extractIndexFields)
+// 					.map(addResourceStats)
+// 					.sequence()		
+// 					.map(updateAgentsCollection)
+// 					.sequence()					
+// 					.batch(500)
+// 					.map(x =>{
+// 						total = total + x.length
+// 						//console.log(total)
+// 						process.send({ totalUpdate: x.length })
+// 						return x
+// 					})					
+// 					.map(_.curry(db.indexAgents))
+// 					.nfcall([])
+// 					.series()
+
+// 					.done(function(err){
+// 						if (err) console.log(err)						
+// 						console.log("Done")
+// 						process.exit()
+// 					})
+// 			}
+// 		})
+// 	})
+// }
+
+
+
+
+
+
+
+
+
+// // var donut = contrib.donut({
+// // 	label: 'INDEXING AGENTS',
+// // 	radius: 12,
+// // 	arcWidth: 6,
+// // 	spacing: 0.5,
+// // 	yPadding: 1,
+// // 	data: [
+// // 	  {percent: 80, label: 'web1', color: 'green'},
+// // 	  {percent: 80, label: 'web2', color: 'blue'},
+// // 	  {percent: 80, label: 'web2', color: 'blue'},
+// // 	  {percent: 80, label: 'web2', color: 'blue'},
+// // 	  {percent: 80, label: 'web3', color: 'red'}
+// // 	]
+// // });	
+
+// // screen.append(donut)
+// // screen.key(['C-c'], function(ch, key) {
+// //  return process.exit(0);
+// // });
+
+// // screen.render()
